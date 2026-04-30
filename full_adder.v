0943befdbbd7ace236449dbd78722ecd83b1a900
danielcolwellbsu <danielcolwell@u.boisestate.edu>
2026-04-30 12:20:49 -0600

    Update full_adder.v

--- full_adder.v
+++ full_adder.v
@@ -6,11 +6,13 @@ module full_adder(
     output Cout
 );
 
+    //combinatorial logic assignment for output Y
     assign Y = (~A & ~B & Cin) |
                 (~A & B & ~Cin) |
                 (A & B & Cin) |
                 (A & ~B & ~Cin);
-     
+
+    //combinatorial logic assignment for carry out
     assign Cout = (A & B) |
                     (B & Cin) |
                     (A & Cin);
