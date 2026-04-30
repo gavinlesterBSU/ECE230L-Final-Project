5401d2432b87622a5cfeef44be8036397e2f821e
danielcolwellbsu <danielcolwell@u.boisestate.edu>
2026-04-30 12:29:43 -0600

    Update moddown.v

--- moddown.v
+++ moddown.v
@@ -8,14 +8,15 @@ module moddown(
     output reg Co
 );
 
+    //initializes positive clock edge test
 always @(posedge clk or posedge rst) begin
-    if (rst) begin
+    if (rst) begin //assigns state to 0 if reset is active
         m <= 6'd0;
         Co <= 0;
-    end else if (load) begin
+    end else if (load) begin //assigns state to load value if load is active
         m <= load_value;
         Co <= 0;
-    end else if (en) begin
+    end else if (en) begin //increments state down while enable is active, stopping at output = 0
         if (m == 6'd0) begin
             m <= 6'd0;
             Co <= 1;
