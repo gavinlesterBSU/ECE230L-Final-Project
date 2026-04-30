16f1f45f1ccd9bd591e6c42a138fab519f6db641
danielcolwellbsu <danielcolwell@u.boisestate.edu>
2026-04-30 12:19:27 -0600

    Update dff.v

--- dff.v
+++ dff.v
@@ -4,12 +4,12 @@ module dff(
     output NotQ
 );
 
-    initial begin
+    initial begin //assigns Q to 0 when the program is initialized
             Q <= 0;
             
     end
 
-    always @(posedge clk) begin 
+    always @(posedge clk) begin //checks if reset is active, otherwise sets output equal to D
         if (rst)
             Q <= 1'b0;
         else
